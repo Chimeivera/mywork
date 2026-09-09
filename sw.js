3811f6510a092eb804732d0b39600e053da7178a
Chimeivera <a101w6@chimei.org.tw>
2026-09-09 13:18:53 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='work-manager-v9-journal-report';
+const CACHE='work-manager-v10-browser-excel';
 const STATIC_ASSETS=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
